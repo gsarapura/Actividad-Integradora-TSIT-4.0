2b8c11b67c3f045facb3170a6ba5488cb8fbac76
HernánQuintana <84577347+HernanCastle@users.noreply.github.com>
2022-06-25 12:51:17 -0300

    Listo el input. Basicamente lo que hace es meter en la lista el valor que le indiquemos y esto se repite 5 veces por el bucle for.

--- funciones.py
+++ funciones.py
@@ -1,3 +1,18 @@
-a  = 1
-print(a)
+
+lista = []
+# descomentar está linea si se quiere decidir la cantidad de números a ingresar
+#cant = int(input('¿Cuántos números quiere ingresar? '))
+#en caso de quere decidir la cantidad de número a agregar en el array, cambiar el parámetro del range por "cant".
+
+for w in  range(5): #el range le dira al for cuantas veces ejecutar el código según el valor que se le indique
+    w = int(input('Ingrese un número: '))
+    lista.append(w)
+    print(lista)
+
+
+
+
+
+
+
 
