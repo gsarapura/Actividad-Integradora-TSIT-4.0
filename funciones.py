70a59f5377246d0f492b9b5323904870d352b5a3
Gustavo Sarapura <gustavo.sarapura7@gmail.com>
2022-06-25 11:01:02 -0300

    Agregar la función promedio de 5 enteros

--- funciones.py
+++ funciones.py
@@ -1,3 +1,9 @@
-a  = 1
-print(a)
-
+def promedio(lista):
+    """
+    Recibe una lista como argumento y retorna el promedio de sus elementos.
+    """
+    acumulador = 0
+    for i in lista:
+        acumulador += i
+    promedio = acumulador // len(lista)
+    return promedio
