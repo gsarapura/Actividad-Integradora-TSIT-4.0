285a3e79be5ac5c7eb4b2a61b38c2fc27a814d39
Lucas-Balduzzi <106196812+Lucas-Balduzzi@users.noreply.github.com>
2022-06-25 16:01:39 -0300

    Integración: Lista, Mínimo, Máximo

--- funciones.py
+++ funciones.py
@@ -1,20 +1,19 @@
 
-lista = []
+list_num = []
 # descomentar está linea si se quiere decidir la cantidad de números a ingresar
 #cant = int(input('¿Cuántos números quiere ingresar? '))
 #en caso de quere decidir la cantidad de número a agregar en el array, cambiar el parámetro del range por "cant".
 
 for w in  range(5): #el range le dira al for cuantas veces ejecutar el código según el valor que se le indique
     w = int(input('Ingrese un número: '))
-    lista.append(w)
-    print(lista)
-
+    list_num.append(w)
     
+print(list_num)
+   
 def min_num_list(my_list):
     """Entra una lista, 
     Retorna:valor mínimo de la lista
     """
-
     min_number = my_list[0]
     for i in my_list:
         if i < min_number:
@@ -23,8 +22,16 @@ def min_num_list(my_list):
 
 # Maximo pasado a funcion
 def largest_num_list(my_list):
+    """Entra una lista, 
+    Retorna:valor máximo de la lista
+    """
     largest_number = my_list[0] #Aquí dejar el commit, zero tambien es válido, pero no esta en la lista
     for i in my_list[1:]:
         if i > largest_number:
             largest_number = i
     return(largest_number)
+
+min_num_list(list_num)
+largest_num_list(list_num)
+print(min_num_list(list_num))
+print(largest_num_list(list_num))
