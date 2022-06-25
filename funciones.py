9906f855d2a7491545d5727a63ac44608fffa77f
Lucas-Balduzzi <106196812+Lucas-Balduzzi@users.noreply.github.com>
2022-06-25 15:13:33 -0300

    CodeMaximo- Pasado a funcion

--- funciones.py
+++ funciones.py
@@ -1,13 +1,6 @@
-#Inicio prueba, limpiar commits "TAREA UNO_LIStA"
-#Cambiar, valor a "LARGEST_NUMBER" segun branch_input
-#num1 = int(input("Ingrese primer digito: "))
-#num2 = int(input("Ingrese segundo digito: "))
-#num3 = int(input("Ingrese tercero digito: "))
-#num4 = int(input("Ingrese cuarto digito: "))
-#num5 = int(input("Ingrese quinto digito: "))
-#my_list = [num1, num2, num3, num4, num5]
-largest_number = num1 #Aquí dejar el commit, zero tambien es válido, pero no esta en la lista
-for i in my_list[1:]:
-    if i > largest_number:
-        largest_number = i
-print(largest_number)
+def largest_num_list(my_list):
+    largest_number = my_list[0] #Aquí dejar el commit, zero tambien es válido, pero no esta en la lista
+    for i in my_list[1:]:
+        if i > largest_number:
+            largest_number = i
+    return(largest_number)
