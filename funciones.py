ab9fff7d7f476b5cdba81c9644ba39611b81dfec
Lucas-Balduzzi <106196812+Lucas-Balduzzi@users.noreply.github.com>
2022-06-27 05:09:55 -0300

    Correciones finales a revisación grupal. Reposicionamiento de funciones.

--- funciones.py
+++ funciones.py
@@ -20,16 +20,16 @@ def sumar(my_list):
         suma += i
     return suma
 
-# Mínimo
-def min_num_list(my_list):
-    """ 
-    Retorna: valor mínimo de la lista
+# Promedio
+def promedio(lista):
     """
-    min_number = my_list[0]
-    for i in my_list:
-        if i < min_number:
-            min_number = i
-    return(min_number)
+    Retorna: el promedio de todos los parámetros en la lista.
+    """
+    acumulador = 0
+    for i in lista:
+        acumulador += i
+    promedio = acumulador // len(lista)
+    return promedio
 
 # Máximo
 def largest_num_list(my_list):
@@ -42,16 +42,17 @@ def largest_num_list(my_list):
             largest_number = i
     return(largest_number)
 
-# Promedio
-def promedio(lista):
+# Mínimo
+def min_num_list(my_list):
+    """ 
+    Retorna: valor mínimo de la lista
     """
-    Retorna: el promedio de todos los parámetros en la lista.
-    """
-    acumulador = 0
-    for i in lista:
-        acumulador += i
-    promedio = acumulador // len(lista)
-    return promedio
+    min_number = my_list[0]
+    for i in my_list:
+        if i < min_number:
+            min_number = i
+    return(min_number)
+
 
 #Éste es el código del menu. Utiliza una cadena de elif para ejecutar el código de la opcion elegida
 option = int(input('Elija el número de las Siguentes opciones: \n '
@@ -59,8 +60,8 @@ option = int(input('Elija el número de las Siguentes opciones: \n '
 ------------------------------ MENU ------------------------------
     1. Función Suma
     2. Función Promedio
-    3. Función Mínimo
-    4. Función Máximo
+    3. Función Máximo
+    4. Función Mínimo
 -------------------------------------------------------------------
     """))
 
@@ -72,11 +73,11 @@ elif option == 2:  #Promedio
     #ejecuto la funcion promedio ásandole de parámetro la lista de números
     print('El Promedio de los números que ingresaste es: ', promedio(list_num))
 
-elif option == 3:  #Mínimo
-    print('El Mínimo valor de la lista es: ',min_num_list(list_num))
-
-elif option == 4:  #Máximo
+elif option == 3:  #Máximo
     print('El Máximo valor de la lista es: ', largest_num_list(list_num))
+
+elif option == 4:  #Mínimo
+    print('El Mínimo valor de la lista es: ',min_num_list(list_num))
 
 else:
     print('Has elegido incorrectamente. Por favor vuelva a intentarlo. ')
